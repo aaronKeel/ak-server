c4b4fafd5ff011049466ccb47640d2ac1af49833
aaronKeel <aaronckeel13@gmail.com>
2020-06-14 23:29:59 -0700

    refine route

--- src/server.ts
+++ src/server.ts
@@ -20,7 +20,10 @@ export const server = (port: number | string): void => {
 
     express.route('/test-ghs')
         .get(async (req: Express.Request, res: Express.Response) => {
-            const rows = await pgClientConnect<Name>('SELECT * FROM public.ghs;');
+            const q = `SELECT location_name, year, the_total_mean, the_total_lower, the_total_upper
+            FROM akserver.ghs
+            WHERE location_name = 'Belgium';`;
+            const rows = await pgClientConnect<Name>(q);
             res.json(rows);
         });
 
